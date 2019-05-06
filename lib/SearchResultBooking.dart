da64675410134e2816cd19426f7b04e327df29f9
DawidStefaniak <contact@dawidstefaniak.com>
2019-05-06 14:31:23 +0100

    Added calendar

--- lib/SearchResultBooking.dart
+++ lib/SearchResultBooking.dart
@@ -1,6 +1,7 @@
 import 'package:barber/Models/Font.dart';
 import 'package:barber/json/response.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
 
 
 class SearchResultBooking extends StatefulWidget{
@@ -17,7 +18,16 @@ class _SearchResultBookingState extends State<SearchResultBooking>{
   @override
   Widget build(BuildContext context) {
 
-    return Container();
+    return Container(
+      child: CalendarCarousel(
+        firstDayOfWeek: 0,
+        selectedDayButtonColor: Color(0xff685408),
+        todayButtonColor: Colors.black54,
+        iconColor: Colors.black54,
+        headerTextStyle: TextStyle(color: Colors.black87, fontSize: 22),
+        //onDayPressed: (DateTime date, List<Event> events){}
+    )
+    );
 
   }
 
