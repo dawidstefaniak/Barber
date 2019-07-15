67e90e1c88f2b251f9bf7ea6b1b33a4ea3ed84c9
Dawid <contact@dawidstefaniak.com>
2019-07-15 16:14:45 +0100

    Added Add Barber design

--- lib/AddBarber.dart
+++ lib/AddBarber.dart
@@ -20,6 +20,8 @@ class _AddBarberState extends State<AddBarber> {
     );
   }
 
+  bool _isButtonEnabled;
+
   TextEditingController _nameController;
   TextEditingController _addressController;
   TextEditingController _phoneController;
@@ -27,36 +29,94 @@ class _AddBarberState extends State<AddBarber> {
   @override
   void initState() {
     super.initState();
+
+    _isButtonEnabled = false;
+
+    // Init controllers
     _nameController = new TextEditingController();
     _addressController = new TextEditingController();
     _phoneController = new TextEditingController();
+
+    // Init listeners
+    _nameController.addListener(_textEditingListener);
+    _addressController.addListener(_textEditingListener);
+    _phoneController.addListener(_textEditingListener);
+  }
+
+  void _textEditingListener() {
+    if (_nameController.text != '' &&
+        _addressController.text != '' &&
+        _phoneController.text != '')
+      _isButtonEnabled = true;
+    else
+      _isButtonEnabled = false;
   }
 
   Widget _getBody() {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          child: ListView(
-            children: <Widget>[
-              TextFieldObject.getTextField(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFieldObject.getTextField(
                   hintText: 'eg. SB Barber Academy',
                   labelText: 'Name',
                   controller: _nameController),
-              TextFieldObject.getTextField(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFieldObject.getTextField(
                   hintText: 'eg. SR5 2AS',
                   labelText: 'Address',
-                  controller: _addressController),
-              TextFieldObject.getTextField(
+                  controller: _addressController,),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFieldObject.getTextField(
                   hintText: 'eg. +44 7730 192348',
                   labelText: 'Phone Number',
-                  controller: _phoneController),
-              RaisedButton(
-                child: Text('Add'),
-                onPressed: _addBarber,
-              )
-            ],
-          ),
+                  controller: _phoneController,
+                  inputType: TextInputType.phone),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              decoration: BoxDecoration(boxShadow: [
+              _isButtonEnabled ?
+                BoxShadow(
+                    offset: Offset(3, 3),
+                    color: Colors.yellow.withOpacity(0.4),
+                    blurRadius: 5)
+               : BoxShadow(
+                    offset: Offset(3, 3),
+                    color: Colors.grey.withOpacity(0.2),
+                    blurRadius: 5)
+              ], border: Border.all(style: BorderStyle.none)),
+              
+              
+              width: MediaQuery.of(context).size.width * 0.25,
+              height: 60,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: MaterialButton(
+                  child: Text(
+                    'Add',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: Font.secondFont,
+                        letterSpacing: 2),
+                  ),
+                  color: Colors.yellow,
+                  splashColor: Colors.white,
+                  disabledColor: Colors.grey[300],
+                  onPressed: _isButtonEnabled ? _addBarber : null,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -73,7 +133,12 @@ class _AddBarberState extends State<AddBarber> {
     if (_phoneController.text.isNotEmpty &&
         _nameController.text.isNotEmpty &&
         _addressController.text.isNotEmpty) {
-          Barber barber = new Barber(_nameController.text, _addressController.text, "scissors.png",_phoneController.text,["barber1.jpg", "barber2.jpg"]);
+      Barber barber = new Barber(
+          _nameController.text,
+          _addressController.text,
+          "scissors.png",
+          _phoneController.text,
+          ["barber1.jpg", "barber2.jpg"]);
       Firestore.instance
           .collection('barbers')
           //TODO use Barber object instead
--- lib/Objects/TextFieldObject.dart
+++ lib/Objects/TextFieldObject.dart
@@ -1,10 +1,33 @@
+import 'package:barber/Models/Font.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class TextFieldObject extends Object{
-  static Widget getTextField({String hintText, String labelText, TextEditingController controller}) {
+class TextFieldObject extends Object {
+  static Widget getTextField(
+      {String hintText, String labelText, TextEditingController controller, TextInputType inputType}) {
     return TextField(
-      decoration: InputDecoration(hintText: hintText??'', labelText: hintText??'',),
       controller: controller,
+      cursorColor: Colors.black,
+      textInputAction: TextInputAction.go,
+      keyboardType: inputType ?? TextInputType.text,
+      style: TextStyle(
+          color: Color(0xff685408), fontFamily: Font.secondFont, letterSpacing: 2.5),
+      decoration: InputDecoration(
+          hintText: hintText ?? '',
+          labelText: labelText ?? '',
+          labelStyle: TextStyle(fontSize: 18, color: Color(0xff685408)),
+          hintStyle: TextStyle(color: Color(0xff685408), letterSpacing: 1),
+          border: _textBorder(),
+          enabledBorder: _textBorder(),
+          focusedBorder: _textBorder(),
+
+          ),
     );
+  }
+
+  static _textBorder() {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(color: Color(0xff685408)));
   }
 }
